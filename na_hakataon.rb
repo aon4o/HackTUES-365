d92c4c3d4d532203f8a230281d0bdf1ce4b4a41d
Antonio <antonio.kasabov@gmail.com>
2019-03-15 11:14:35 +0200

    Adding Images Folder
    Added a folder with images.

--- na_hakataon.rb
+++ na_hakataon.rb
@@ -1,11 +1,29 @@
 require 'gosu'
 
+module Zlay
+  BACKGROUND = 0
+  CURSOR = 10
+end
+
+
+
 class TheGame < Gosu::Window
   def initialize
     super 640, 480
+
+    @background = Gosu::Image.new("na_haka_snimki/grey_back.png")
+    @cursor = Gosu::Image.new("na_haka_snimki/cursor.png")
   end
 
 
+  def update
+    #...
+  end
+
+  def draw
+    @background.draw(0, 0, Zlay::BACKGROUND)
+    @cursor.draw(self.mouse_x, self.mouse_y, Zlay::CURSOR)
+  end
 
 end
 
