22b73035a862a5d2121044db52f5d7d7c5830224
Antonio <antonio.kasabov@gmail.com>
2019-03-15 11:54:59 +0200

    Room with walls
    Made a demo room with walls.

--- na_hakataon.rb
+++ na_hakataon.rb
@@ -2,9 +2,26 @@ require 'gosu'
 
 module Zlay
   BACKGROUND = 0
+  WALLS = 1
   CURSOR = 10
 end
 
+
+class Wall
+  def initialize(x, y, w, h)
+    @x = x
+    @y = y
+    @width = w
+    @height = h
+
+    @img = Gosu::Image.new("na_haka_snimki/stena_kvadrat.png")
+  end
+
+  def draw
+    @img.draw_rot(@x, @y, Zlay::WALLS,  0, center_x = 0.5, center_y = 0.5, scale_x = @width, scale_y = @height)
+  end
+
+end
 
 
 class TheGame < Gosu::Window
@@ -13,6 +30,12 @@ class TheGame < Gosu::Window
 
     @background = Gosu::Image.new("na_haka_snimki/grey_back.png")
     @cursor = Gosu::Image.new("na_haka_snimki/cursor.png")
+
+    @walls = [Wall.new(0, self.height/2, 0.5, 5),
+      Wall.new(self.width/2, 0, 7, 0.5),
+      Wall.new(self.width, self.height/2, 0.5, 5),
+      Wall.new(self.width/2, self.height, 7, 0.5)]
+
   end
 
 
@@ -23,6 +46,7 @@ class TheGame < Gosu::Window
   def draw
     @background.draw(0, 0, Zlay::BACKGROUND)
     @cursor.draw(self.mouse_x, self.mouse_y, Zlay::CURSOR)
+    @walls.each{|wall| wall.draw}
   end
 
 end
