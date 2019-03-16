8afe087e2c3dd47f0f17304357aef140fce5fad6
aon2003 <a.o.naidenov@gmail.com>
2019-03-16 11:34:06 +0200

    Added the base of the animation of the player

--- player.rb
+++ player.rb
@@ -4,6 +4,7 @@ class Player
     @x = x
     @y = y
     @img = Gosu::Image.new("na_haka_snimki/Red.jpg")
+    @player_anim = Gosu::Image.load_tiles("na_haka_snimki/4avek_nadqsno.png", 76, 62)
   end
 
   def goto(x, y)
@@ -26,8 +27,8 @@ class Player
   def go_right
     @x += 8
   end
-
   def draw
-    @img.draw_rot(@x, @y, Zlay::PLAYER, 0)
+    player = @player_anim[Gosu.milliseconds/100%@player_anim.size]
+    player.draw_rot(@x, @y, Zlay::PLAYER, 0)
   end
 end
