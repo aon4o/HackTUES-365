3cafc4aff5fe5d17e586414decbb0e7a485470fd
aon2003 <a.o.naidenov@gmail.com>
2019-03-16 10:05:53 +0200

    Changed some little things

--- questions_frame.rb
+++ questions_frame.rb
@@ -30,19 +30,18 @@ class Tutorial < Gosu::Window
   end
 
   def update
-    if (Gosu.distance(@opt_a.x, @opt_a.y, mouse_x, mouse_y) < 15 && button_down?(Gosu::MsLeft))
+    if (Gosu.distance(@opt_a.x+10, @opt_a.y+10, mouse_x, mouse_y) < 20 && button_down?(Gosu::MsLeft))
       puts("you cklicked opt a")
     end
-    if (Gosu.distance(@opt_b.x, @opt_b.y, mouse_x, mouse_y) < 15 && button_down?(Gosu::MsLeft))
+    if (Gosu.distance(@opt_b.x+10, @opt_b.y+10, mouse_x, mouse_y) < 20 && button_down?(Gosu::MsLeft))
       puts("you cklicked opt b")
     end
-    if (Gosu.distance(@opt_c.x, @opt_c.y, mouse_x, mouse_y) < 15 && button_down?(Gosu::MsLeft))
+    if (Gosu.distance(@opt_c.x+10, @opt_c.y+10, mouse_x, mouse_y) < 20 && button_down?(Gosu::MsLeft))
       puts("you cklicked opt c")
     end
   end
 
   def draw
-    #@background_image.draw(0,0,0)
     @cursor.draw(self.mouse_x, self.mouse_y, 10)
     @menu.draw_rot(150,450,2,0,0.5,0.5,0.1,0.1)
     @font.draw("QUESTION HERE", 210, 30, 5, 3, 3, Gosu::Color::BLACK)
